94606938dc248ab303e8f122684eef69fd093289
SUMIT KUMAR <sumitkum171201@gmail.com>
2024-10-04 21:21:42 +0530

    Update basicofset.js
    intro of set

--- Set object/basicofset.js
+++ Set object/basicofset.js
@@ -1,1 +1,6 @@
+// set is used to store the elements with unique values . The values can be of any type whether primitive or object refrence 
+let set1= new Set([1,2,2,3,4,5,6]) //creating a set in javascript
+console.log(set1);//this will give output in the form of set to change it in array we use ... operator output: Set(6) { 1, 2, 3, 4, 5, 6 }
+console.log([...set1]); // ouput : [ 1, 2, 3, 4, 5, 6 ]
+
 
